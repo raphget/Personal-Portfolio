529f8e1fec91e7fe6ca62b8a86d3f4f7d4c918e7
Raphael Getachew <rapha@Raphaels-MacBook-Pro.local>
2023-01-26 22:02:00 -0500

    contact updates

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -27,19 +27,14 @@ export default function Contact() {
   return (
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-        <div className="lg:w-1/2 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-            
-          </div>
-        </div>
         <form
           netlify
           name="test"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+          <h1 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Contact Me
-          </h2>
+          </h1>
           <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
