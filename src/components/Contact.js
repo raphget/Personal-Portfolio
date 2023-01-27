8ccaf83f2350ac05cbb8617474681ab96cef3b5a
Raphael Getachew <rapha@Raphaels-MacBook-Pro.local>
2023-01-26 22:00:15 -0500

    contact updates

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -43,7 +43,7 @@ export default function Contact() {
           <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed">
+              <a className="text-white-400 leading-relaxed">
                 dev.raphget@email.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
