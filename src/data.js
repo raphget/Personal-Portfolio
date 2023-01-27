5bd48aeeaf8e0bd2200194d2c97343ea42ffb0df
Raphael Getachew <rapha@Raphaels-MacBook-Pro.local>
2023-01-26 23:33:50 -0500

    contact updates

--- src/data.js
+++ src/data.js
@@ -33,26 +33,15 @@ export const projects = [
   },
 ];
 
-export const testimonials = [
-  {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "Jesse Hicks",
-    company: "Zoozle",
-  },
-  {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
-    name: "Ruben Alvarez",
-    company: "Dooble.io",
-  },
-];
 
 export const skills = [
-  "Java",
-  "Python",
+  "Java (JSP/Servlets, Swing, SpringBoot, JDBC)",
+  "Python (Numpy, Pandas, Scikit/Sklearn, Tensorflow, Selenium)",
+  "C/C++",
+  "JavaScript",
+  "HTML/CSS",
   "SQL",
-  "React",
+  "GIT",
+  
+  
 ];
