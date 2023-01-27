2b10977226f0f9690917fa138ce476a18d96d0b6
Raphael Getachew <rapha@Raphaels-MacBook-Pro.local>
2023-01-26 22:04:52 -0500

    contact updates

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,7 +26,7 @@ export default function Contact() {
 
   return (
     <section id="contact" className="relative">
-      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap w-full">
         <form
           netlify
           name="test"
