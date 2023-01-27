c4b3055a45936410bc9de96cf1493e6dee422258
Raphael Getachew <rapha@Raphaels-MacBook-Pro.local>
2023-01-26 22:59:21 -0500

    contact updates

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <section id="about">
       <div className="container mx-auto flex px-30 py-20 md:flex-row flex-col items-center">
-        <div className="pl-50 lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+        <div className="pr-50 lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Raphael.
             <br className="hidden lg:inline-block" />Full Stack Developer.
