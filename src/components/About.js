1d0015e9a265132045dcdcec27cf7cdbd80e70a1
Raphael Getachew <rapha@Raphaels-MacBook-Pro.local>
2023-01-26 23:13:01 -0500

    contact updates

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function About() {
   return (
     <section id="about">
-      <div className="container mx-auto flex px-30 py-20 md:flex-row flex-col items-center">
+      <div className="container mx-auto flex px-20 py-20 md:flex-row flex-col items-center">
         <div className="pr-50 lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Raphael.
@@ -14,7 +14,7 @@ export default function About() {
           <div className="flex justify-center">
             <a
               href="#contact"
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+              className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
               Contact Info
             </a>
             <a
