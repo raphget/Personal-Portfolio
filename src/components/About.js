b247401014a1dbb9a284a56e3859183e6939d991
Raphael Getachew <rapha@Raphaels-MacBook-Pro.local>
2023-01-26 22:54:18 -0500

    contact updates

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ export default function About() {
     <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+          <h1 className="pl-15 title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Raphael.
             <br className="hidden lg:inline-block" />Full Stack Developer.
           </h1>
@@ -15,7 +15,7 @@ export default function About() {
             <a
               href="#contact"
               className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-              Contact
+              Contact Info
             </a>
             <a
               href="#projects"
