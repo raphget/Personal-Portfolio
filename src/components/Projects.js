3ed045dabeca95745ee2c423b91480874ba301ca
Raphael Getachew <rapha@Raphaels-MacBook-Pro.local>
2023-01-30 21:52:21 -0500

    First Draft (need to finish projects section)

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,4 @@
-import { CodeIcon } from "@heroicons/react/solid";
+import { ChevronDownIcon } from "@heroicons/react/solid";
 import React from "react";
 import { projects } from "../data";
 
@@ -7,7 +7,7 @@ export default function Projects() {
     <section id="projects" className="text-gray-400 body-font">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
-          <CodeIcon className="mx-auto inline-block w-10 mb-4" />
+          <ChevronDownIcon className="mx-auto inline-block w-10 mb-4" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
             My Work
           </h1>
