2370a7f22cf50a1734e566213583c04768d31fcd
Raphael Getachew <rapha@Raphaels-MacBook-Pro.local>
2023-02-14 14:19:25 -0500

    jdbc demo placeholder

--- src/data.js
+++ src/data.js
@@ -11,8 +11,8 @@ export const projects = [
     title: "CUSTOMER LOYALTY PROGRAM",
     subtitle: "JDBC; Oracle PL/SQL; Java Swing",
     description:
-      "Created a Java Swing application to maintain the database system for a grocery store customer loyalty program. The Swing interface allows the user to view and manage data stored in a relational database.",
-    image: "./mapleappdemo.gif",
+      "Created a Java application to maintain the database system for a grocery store customer loyalty program. The Swing user interface allows a grocery store employee to view and manage data stored in a relational database.",
+    image: "./java_jdbc.jpeg",
     link: "#projects",
   },
 ];
