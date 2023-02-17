a3076ff36e0d0b995c152f60ede59ec066f3c71f
Raphael Getachew <rapha@Raphaels-MacBook-Pro.local>
2023-02-17 12:37:16 -0500

    placeholder gif

--- src/data.js
+++ src/data.js
@@ -12,7 +12,7 @@ export const projects = [
     subtitle: "JDBC; Oracle PL/SQL; Java Swing",
     description:
       "Created a Java application to maintain the database system for a grocery store customer loyalty program. The Swing user interface allows a grocery store employee to view and manage data stored in a relational database.",
-    image: "./java_jdbc.jpeg",
+    image: "./giphy.gif",
     link: "#projects",
   },
 ];
