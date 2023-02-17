39a290ff6d5cdd5bb483b41abe34c5952b831771
Raphael Getachew <rapha@Raphaels-MacBook-Pro.local>
2023-02-17 15:45:48 -0500

    more placeholder gifs

--- src/data.js
+++ src/data.js
@@ -12,11 +12,42 @@ export const projects = [
     subtitle: "JDBC; Oracle PL/SQL; Java Swing",
     description:
       "Created a Java application to maintain the database system for a grocery store customer loyalty program. The Swing user interface allows a grocery store employee to view and manage data stored in a relational database.",
-    image: "./giphy.gif",
+    image: "./giphylangs.gif",
     link: "#projects",
   },
+  {
+    title: "IMAGE CLASSIFICATION",
+    subtitle: "Python; Tensorflow (Keras); Numpy; Sklearn",
+    description:
+      "Built a three layer feedforward network to classify hand drawn images of digits (0 - 9). Across 10,000 test images the best model was able classify hand-drawn images with an accuracy.",
+    image: "./giphycat.gif",
+    link: "https://github.com/raphget/DigitClassify",
+  },
+  {
+    title: "KNN SENTIMENT ANALYSIS",
+    subtitle: "Python; Sklearn; NLTK; Data Science",
+    description:
+      "Implemented a k-Nearest Neighbor Classifier to predict the sentiment for various amazon reviews. Used cross-validation resampling to test and train the model on different iterations.",
+    image: "./giphycat.gif",
+    link: "https://github.com/raphget/",
+  },
+  {
+    title: "VACATION PLANNER WEBAPP",
+    subtitle: "HTML/CSS; JavaScript; Google API",
+    description:
+      "Collaborated with a small team to build a DMV Vacation Planner Web Application. Implemented the sorting algorithms, and incorporated Google’s Geocoder API for location data.",
+    image: "./giphycode.gif",
+    link: "https://github.com/kacgomez/cs321-project",
+  },
+  {
+    title: "MONOPOLY SIMULATION",
+    subtitle: "Python; MatPlotLib; Numpy",
+    description:
+      "Created a simulation of  the classic game of Monopoly in order to test the effectiveness of various playstyles. Used matplotlib to visualize win rates across various playstyles.",
+    image: "./giphycode.gif",
+    link: "https://github.com/raphget/MonopolySim",
+  },
 ];
-
 
 
 export const skills = [
